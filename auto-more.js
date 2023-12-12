408de541ee96c222c3e731f8f00f9566184e4254
Steve Pflug <stephen@pflug.us>
2023-12-12 17:21:29 -0600

    updated script to work with setTimout

--- auto-more.js
+++ auto-more.js
@@ -1,12 +1,21 @@
-onload(() => {
+console.log('auto-more.js loaded');
+setTimeout(() => {
+    console.log('Inside auto-more.js');
     const sidebarCollapseToggleBtn = document.querySelector('.collapse-toggle').querySelector('button');
-    const showMoreBtn = document.querySelector('.side-nav-show-more-toggle__button').querySelector('button');
-    let userPrefOpen = false;
+    console.log(sidebarCollapseToggleBtn);
+    let showMoreDiv = !!document.querySelector('.side-nav-show-more-toggle__button');
+    console.log(showMoreDiv);
+    console.log(!showMoreDiv);
+    let userPrefOpen = true;
 
-    if(!showMoreBtn) {
+    if(!showMoreDiv) {
+        console.log('!showMoreDiv is false, opening sidebar');
         sidebarCollapseToggleBtn.click();
-        userPrefOpen = true;
+        userPrefOpen = false;
+        console.log(`userPrefOpen is now: ${userPrefOpen}`);
     }
+    showMoreDiv = document.querySelector('.side-nav-show-more-toggle__button');
+    const showMoreBtn = showMoreDiv.querySelector('button');
     showMoreBtn.click();
     if(!userPrefOpen) sidebarCollapseToggleBtn.click();
-});
+}, 1000);
