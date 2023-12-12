557fa55ce249f180afbe61fa282d3fc553ee69d3
Steve Pflug <stephen@pflug.us>
2023-12-12 16:45:51 -0600

    updated script to run onload

--- auto-more.js
+++ auto-more.js
@@ -1,10 +1,12 @@
-const sidebarCollapseToggleBtn = document.querySelector('.collapse-toggle').querySelector('button');
-const showMoreBtn = document.querySelector('.side-nav-show-more-toggle__button').querySelector('button');
-let userPrefOpen = false;
+onload(() => {
+    const sidebarCollapseToggleBtn = document.querySelector('.collapse-toggle').querySelector('button');
+    const showMoreBtn = document.querySelector('.side-nav-show-more-toggle__button').querySelector('button');
+    let userPrefOpen = false;
 
-if(!showMoreBtn) {
-    sidebarCollapseToggleBtn.click();
-    userPrefOpen = true;
-}
-showMoreBtn.click();
-if(!userPrefOpen) sidebarCollapseToggleBtn.click();
+    if(!showMoreBtn) {
+        sidebarCollapseToggleBtn.click();
+        userPrefOpen = true;
+    }
+    showMoreBtn.click();
+    if(!userPrefOpen) sidebarCollapseToggleBtn.click();
+});
