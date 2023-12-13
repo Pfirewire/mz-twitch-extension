1947c789587d4d7ea1a397cc1dbea5eae4392090
Steve Pflug <stephen@pflug.us>
2023-12-13 11:57:52 -0600

    updated code to extract logic to smaller functions

--- auto-more.js
+++ auto-more.js
@@ -4,24 +4,34 @@ if(startingScreenWidth < 1200) {
     console.log("Unable to use this extension, window is less than 1200px");
 } else {
     const sidebarCollapseToggleBtn = document.querySelector('.collapse-toggle').querySelector('button');
-    let isShowMoreOnScreen = !!document.querySelector('.side-nav-show-more-toggle__button');
+    let isShowMoreOnScreen = isElTruthy('.side-nav-show-more-toggle__button');
     const userPrefOpen = !!document.querySelector('.side-nav__title');
     let isSidePanelOpen = userPrefOpen;
 
+    function isElTruthy(query) {
+        return !!document.querySelector(query)
+    }
+    function openSidebarIfClosed() {
+        if (!isSidePanelOpen) {
+            sidebarCollapseToggleBtn.click();
+        }
+    }
+    function showMoreIfAble() {
+        if (isShowMoreOnScreen) {
+            const showMoreBtn = document.querySelector('.side-nav-show-more-toggle__button').querySelector('button');
+            showMoreBtn.click();
+            if (!userPrefOpen) sidebarCollapseToggleBtn.click();
+        } else {
+            isSidePanelOpen = !!document.querySelector('.side-nav__title');
+            checkForLoadedBtns();
+        }
+    }
+
     function checkForLoadedBtns() {
         setTimeout(() => {
-            if (!isSidePanelOpen) {
-                sidebarCollapseToggleBtn.click();
-            }
-            isShowMoreOnScreen = !!document.querySelector('.side-nav-show-more-toggle__button');
-            if (isShowMoreOnScreen) {
-                const showMoreBtn = document.querySelector('.side-nav-show-more-toggle__button').querySelector('button');
-                showMoreBtn.click();
-                if (!userPrefOpen) sidebarCollapseToggleBtn.click();
-                return;
-            }
-            isSidePanelOpen = !!document.querySelector('.side-nav__title');
-            return checkForLoadedBtns();
+            openSidebarIfClosed();
+            isShowMoreOnScreen = isElTruthy('.side-nav-show-more-toggle__button');
+            showMoreIfAble();
         }, 100);
     }
 
