9607b6ec706779f5c71389d95f00a4967ef08aa8
Steve Pflug <stephen@pflug.us>
2023-12-12 18:23:34 -0600

    added functionality to check multiple times with a time delay for correct DOM buttons

--- auto-more.js
+++ auto-more.js
@@ -1,21 +1,29 @@
-console.log('auto-more.js loaded');
-setTimeout(() => {
-    console.log('Inside auto-more.js');
-    const sidebarCollapseToggleBtn = document.querySelector('.collapse-toggle').querySelector('button');
-    console.log(sidebarCollapseToggleBtn);
-    let showMoreDiv = !!document.querySelector('.side-nav-show-more-toggle__button');
-    console.log(showMoreDiv);
-    console.log(!showMoreDiv);
-    let userPrefOpen = true;
+const startingScreenWidth = window.innerWidth;
 
-    if(!showMoreDiv) {
-        console.log('!showMoreDiv is false, opening sidebar');
-        sidebarCollapseToggleBtn.click();
-        userPrefOpen = false;
-        console.log(`userPrefOpen is now: ${userPrefOpen}`);
+if(startingScreenWidth < 1200) {
+    console.log("Unable to use this extension, window is less than 1200px");
+} else {
+    const sidebarCollapseToggleBtn = document.querySelector('.collapse-toggle').querySelector('button');
+    let isShowMoreOnScreen = !!document.querySelector('.side-nav-show-more-toggle__button');
+    const userPrefOpen = !!document.querySelector('.side-nav__title');
+    let isSidePanelOpen = userPrefOpen;
+
+    function checkForLoadedBtns() {
+        setTimeout(() => {
+            if (!isSidePanelOpen) {
+                sidebarCollapseToggleBtn.click();
+            }
+            isShowMoreOnScreen = !!document.querySelector('.side-nav-show-more-toggle__button');
+            if (isShowMoreOnScreen) {
+                const showMoreBtn = document.querySelector('.side-nav-show-more-toggle__button').querySelector('button');
+                showMoreBtn.click();
+                if (!userPrefOpen) sidebarCollapseToggleBtn.click();
+                return;
+            }
+            isSidePanelOpen = !!document.querySelector('.side-nav__title');
+            return checkForLoadedBtns();
+        }, 100);
     }
-    showMoreDiv = document.querySelector('.side-nav-show-more-toggle__button');
-    const showMoreBtn = showMoreDiv.querySelector('button');
-    showMoreBtn.click();
-    if(!userPrefOpen) sidebarCollapseToggleBtn.click();
-}, 1000);
+
+    checkForLoadedBtns();
+}
